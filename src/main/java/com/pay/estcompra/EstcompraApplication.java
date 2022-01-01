793842ba0d78051048ba98bbfd3edd497e0742ee
Caio Pereira <caiopereira1998@hotmail.com>
2022-01-01 18:10:47 -0300

    Projeto Criado

---
+++ src/main/java/com/pay/estcompra/EstcompraApplication.java
@@ -0,0 +1,13 @@
+package com.pay.estcompra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstcompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstcompraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pay/estcompra/EstcompraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pay.estcompra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstcompraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
