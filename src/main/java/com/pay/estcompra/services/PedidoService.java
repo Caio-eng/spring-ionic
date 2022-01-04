8bb1d076f61f619d9ac4844009085dd08b68139a
Caio Pereira <caiopereira1998@hotmail.com>
2022-01-03 22:17:56 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/pay/estcompra/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.pay.estcompra.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pay.estcompra.domain.Pedido;
+import com.pay.estcompra.repositories.PedidoRepository;
+import com.pay.estcompra.services.exceptions.ObjectNotFountException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFountException(
+				"Pedido não encontrada! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
