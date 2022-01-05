066396c632e836e14d85d94f2782b48152059fe6
Caio Pereira <caiopereira1998@hotmail.com>
2022-01-05 19:11:19 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/pay/estcompra/resources/CategoriaResource.java
+++ src/main/java/com/pay/estcompra/resources/CategoriaResource.java
@@ -1,11 +1,16 @@
 package com.pay.estcompra.resources;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pay.estcompra.domain.Categoria;
 import com.pay.estcompra.services.CategoriaService;
@@ -17,10 +22,24 @@ public class CategoriaResource {
 	@Autowired
 	CategoriaService service;
 	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Categoria>> findAll() {
+		List<Categoria> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
--- src/main/java/com/pay/estcompra/services/CategoriaService.java
+++ src/main/java/com/pay/estcompra/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.pay.estcompra.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +16,19 @@ public class CategoriaService {
 	@Autowired
 	CategoriaRepository repo;
 	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFountException(
 				"Categoria não encontrada! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
 }
