088c41a76107cd3ba5e67f1a33fee559577f29ad
Caio Pereira <caiopereira1998@hotmail.com>
2022-01-05 20:14:20 -0300

    Listando todas categorias com DTO

--- src/main/java/com/pay/estcompra/resources/CategoriaResource.java
+++ src/main/java/com/pay/estcompra/resources/CategoriaResource.java
@@ -2,6 +2,7 @@ package com.pay.estcompra.resources;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pay.estcompra.domain.Categoria;
+import com.pay.estcompra.dto.CategoriaDTO;
 import com.pay.estcompra.services.CategoriaService;
 
 @RestController
@@ -22,10 +24,18 @@ public class CategoriaResource {
 	@Autowired
 	CategoriaService service;
 	
+	//Categoria Original
+//	@RequestMapping(method = RequestMethod.GET)
+//	public ResponseEntity<List<Categoria>> findAll() {
+//		List<Categoria> list = service.findAll();
+//		return ResponseEntity.ok().body(list);
+//	}
+	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<Categoria>> findAll() {
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
