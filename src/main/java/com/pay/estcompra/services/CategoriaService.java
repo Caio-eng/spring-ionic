e162ec93f766e0e2b3d439d8920fe79217b81b01
Caio Pereira <caiopereira1998@hotmail.com>
2022-01-05 20:49:31 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/pay/estcompra/services/CategoriaService.java
+++ src/main/java/com/pay/estcompra/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.pay.estcompra.domain.Categoria;
@@ -25,6 +28,12 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFountException(
 				"Categoria não encontrada! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerpage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerpage, Direction.valueOf(direction),
+				orderBy);
+		return repo.findAll(pageRequest);
 	}
 	
 	public Categoria insert(Categoria obj) {
