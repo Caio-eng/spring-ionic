597107593ba6d145f7b2313aa399d7b2265249be
Caio Pereira <caiopereira1998@hotmail.com>
2022-01-08 16:33:17 -0300

    Inserindo pedido

--- src/main/java/com/pay/estcompra/domain/PagamentoComCartao.java
+++ src/main/java/com/pay/estcompra/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.pay.estcompra.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.pay.estcompra.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
